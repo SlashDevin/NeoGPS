cd8a79bcdbb1082bcec6ba2ca1c65ac0d5e6df78
SlashDevin <slashdevin@not.really>
2015-09-14 09:02:10 -0400

    Add support for non-Mega boards
    * use SoftwareSerial

--- examples/NMEA/NMEA.ino
+++ examples/NMEA/NMEA.ino
@@ -1,6 +1,13 @@
 /*
-  Serial is for trace output.
-  Serial1 should be connected to the GPS device.
+  Serial is for trace output to the Serial Monitor window.
+
+  For Mega Boards, Serial1 should be connected to the GPS device:
+    pin 18 to the GPS RX pin, and pin 19 to the GPS TX pin.  You can
+    change this to Serial2 or Serial3 (see line 25).
+
+  For all other Boards, pin 3 should be connected to the GPS TX pin, and
+    pin 4 should be connected to the GPS RX pin.  You can change these
+    pin numbers below (see lines 38 and 41).
 */
 
 #include <Arduino.h>
@@ -8,7 +15,42 @@
 #include "NMEAGPS.h"
 #include "Streamers.h"
 
-// Set this to your debug output device.
+//-----------------------------------------------------------
+// Pick the serial port to which the GPS device is connected.
+
+#if defined(UBRR1H)
+
+  // The current Board (a Mega?) has an extra hardware serial port
+  //   on pins 18 (TX1) and 19 (RX1)
+  HardwareSerial & gps_port = Serial1;
+
+  #define USING_GPS_PORT "Serial1"
+
+#else
+
+  // The current Board (an Uno?) does not have an extra serial port.
+  // Use SoftwareSerial to listen to the GPS device.
+  //   You should expect to get some RX errors, which may
+  //   prevent getting fix data every second.  YMMV.
+  #include "SoftwareSerial.h"
+
+  // Arduino RX pin number that is connected to the GPS TX pin
+  #define RX_PIN 3
+
+  // Arduino TX pin number that is connected to the GPS RX pin
+  #define TX_PIN 4
+
+  SoftwareSerial gps_port( RX_PIN, TX_PIN );
+
+  //  Here's a little preprocessor magic to get a nice string for /setup/
+  #define f(x) #x
+  #define STRINGIZE(f,x) f(x)
+  #define USING_GPS_PORT "SoftwareSerial( RX pin " STRINGIZE(f,RX_PIN) ", TX pin " STRINGIZE(f,TX_PIN) " )"
+
+#endif
+
+//------------------------------------------------------------
+// Set this to your debug output device, if it's not "Serial".
 Stream & trace = Serial;
 
 static NMEAGPS gps;
@@ -24,13 +66,13 @@ void setup()
   trace.println( sizeof(gps.fix()) );
   trace.print( F("NMEAGPS object size = ") );
   trace.println( sizeof(NMEAGPS) );
-
+  trace.println( F("Looking for GPS device on " USING_GPS_PORT) );
   trace_header();
 
   trace.flush();
   
   // Start the UART for the GPS device
-  Serial1.begin(9600);
+  gps_port.begin(9600);
 }
 
 //--------------------------
@@ -38,22 +80,17 @@ void setup()
 void loop()
 {
   static uint32_t last_rx = 0L;
-  static gps_fix rmc_data;
+  static gps_fix  rmc_data;
 
-  while (Serial1.available()) {
+  while (gps_port.available()) {
     last_rx = millis();
 
-    if (gps.decode( Serial1.read() ) == NMEAGPS::DECODE_COMPLETED) {
+    if (gps.decode( gps_port.read() ) == NMEAGPS::DECODE_COMPLETED) {
 
-//#ifdef NMEAGPS_SAVE_TALKER_ID
-//trace << gps.talker_id[0] << gps.talker_id[1] << ' ';
-//#endif
-//trace << (uint8_t) gps.nmeaMessage << ' ';
-
-// Make sure that the only sentence we care about is enabled
-#ifndef NMEAGPS_PARSE_RMC
-#error NMEAGPS_PARSE_RMC must be defined in NMEAGPS.h!
-#endif
+      // Make sure that the only sentence we care about is enabled
+      #ifndef NMEAGPS_PARSE_RMC
+        #error NMEAGPS_PARSE_RMC must be defined in NMEAGPS_cfg.h!
+      #endif
 
       if (gps.nmeaMessage == NMEAGPS::NMEA_RMC) {
         rmc_data = gps.fix(); // copied for printing later...
@@ -64,10 +101,26 @@ void loop()
     }
   }
 
+  static uint32_t last_seconds = 0UL; // Remember the last time we printed
+
+  //  Make sure we print *something* every two seconds, even if we
+  //    haven't gotten any valid data.  
+  //  This will show whether we have been getting *any* characters.
+
+  static uint32_t last_time_seconds_changed = 0UL;
+
+  if (millis() - last_time_seconds_changed > 2000UL)
+    seconds += 2;
+
+  if (last_seconds != seconds) {
+    last_seconds = seconds;
+    last_time_seconds_changed = millis();
+  }
+
   // Print things out once per second, after the serial input has died down.
   // This prevents input buffer overflow during printing.
 
-  static uint32_t last_trace = 0L;
+  static uint32_t last_trace = 0UL;
 
   if ((last_trace != seconds) && (millis() - last_rx > 5)) {
     last_trace = seconds;
