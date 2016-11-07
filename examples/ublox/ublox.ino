694e388ffa3273aab2cd9f8eb69f3c7a7912efc7
SlashDevin <slashdevin@not.really>
2016-11-07 10:21:49 -0500

    Check DOP message and fix configs
    * fixes hang when DOP msg enabled and fix disabled

--- examples/ublox/ublox.ino
+++ examples/ublox/ublox.ino
@@ -48,6 +48,10 @@
 //------------------------------------------------------------
 // Check that the config files are set up properly
 
+#ifndef NMEAGPS_DERIVED_TYPES
+  #error You must "#define NMEAGPS_DERIVED_TYPES" in NMEAGPS_cfg.h!
+#endif
+
 #if !defined(UBLOX_PARSE_STATUS) & !defined(UBLOX_PARSE_TIMEGPS) & \
     !defined(UBLOX_PARSE_TIMEUTC) & !defined(UBLOX_PARSE_POSLLH) & \
     !defined(UBLOX_PARSE_VELNED) & !defined(UBLOX_PARSE_SVINFO)  & \
@@ -55,6 +59,20 @@
 
   #error No UBX binary messages enabled: no fix data available for fusing.
 
+#endif
+
+#if defined(UBLOX_PARSE_DOP) & \
+    ( !defined(GPS_FIX_HDOP) & \
+      !defined(GPS_FIX_VDOP) & \
+      !defined(GPS_FIX_PDOP) )
+  #warning UBX DOP message is enabled, but all GPS_fix DOP members are disabled.
+#endif
+
+#ifndef NMEAGPS_RECOGNIZE_ALL
+  //  Resetting the messages with ublox::configNMEA requires that
+  //    all message types are recognized (i.e., the enum has all
+  //    values).
+  #error You must "#define NMEAGPS_RECOGNIZE_ALL" in NMEAGPS_cfg.h!
 #endif
 
 //-----------------------------------------------------------------
@@ -204,10 +222,7 @@ public:
         enabled_msg_with_time = true;
       #endif
 
-      #if (defined(GPS_FIX_HDOP) | \
-           defined(GPS_FIX_VDOP) | \
-           defined(GPS_FIX_PDOP)) & \
-          defined(UBLOX_PARSE_DOP)
+      #if defined(UBLOX_PARSE_DOP)
         if (!enable_msg( ublox::UBX_NAV, ublox::UBX_NAV_DOP ))
           DEBUG_PORT.println( F("enable DOP failed!") );
         else
