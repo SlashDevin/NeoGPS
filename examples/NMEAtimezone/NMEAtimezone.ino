e3cf094fc155a1ebf2b6c0ea21b3880c227ed276
SlashDevin <slashdevin@not.really>
2016-12-28 08:23:36 -0500

    Add US DST calculation to timezone example

--- examples/NMEAtimezone/NMEAtimezone.ino
+++ examples/NMEAtimezone/NMEAtimezone.ino
@@ -62,14 +62,43 @@ static void doSomeWork( const gps_fix & fix )
   // Display the local time
 
   if (fix.valid.time) {
+    NeoGPS::clock_t seconds = fix.dateTime;
+
+    //  USA Daylight Savings Time rule (calculated once per reset and year!)
+    static NeoGPS::time_t  changeover;
+    static NeoGPS::clock_t springForward, fallBack;
+
+    if ((springForward == 0) || (changeover.year != fix.dateTime.year)) {
+      changeover.year    = fix.dateTime.year;
+      changeover.month   = 3;
+      changeover.date    = 14; // latest 2nd Sunday
+      changeover.hours   = 2;
+      changeover.minutes = 0;
+      changeover.seconds = 0;
+      changeover.set_day();
+      // Step back to the 2nd Sunday, if day != SUNDAY
+      changeover.date -= (changeover.day - NeoGPS::time_t::SUNDAY);
+      springForward = (NeoGPS::clock_t) changeover;
+
+      changeover.month   = 11;
+      changeover.date    = 7; // latest 1st Sunday
+      changeover.set_day();
+      // Step back to the 1st Sunday, if day != SUNDAY
+      changeover.date -= (changeover.day - NeoGPS::time_t::SUNDAY);
+      fallBack = (NeoGPS::clock_t) changeover;
+    }
+
     // Set these values to the offset of your timezone from GMT
-    static const int32_t         zone_hours   = -4L; // EST
+    int32_t zone_hours   = -5L; // EST
+    if ((springForward <= seconds) && (seconds < fallBack))
+      zone_hours++;
+
     static const int32_t         zone_minutes =  0L; // usually zero
     static const NeoGPS::clock_t zone_offset  =
                       zone_hours   * NeoGPS::SECONDS_PER_HOUR +
                       zone_minutes * NeoGPS::SECONDS_PER_MINUTE;
 
-    NeoGPS::time_t localTime( fix.dateTime + zone_offset );
+    NeoGPS::time_t localTime( seconds + zone_offset );
 
     DEBUG_PORT << localTime;
   }
