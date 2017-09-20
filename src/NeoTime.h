d3f96eb7e8c72f0627d84e1e02c04ed94590ba78
Christian Loitsch <christian@loitsch.com>
2017-09-20 20:12:50 +0200

    Revert "doc: fix minor documentation mistakes"
    This reverts commit 8a1ec52b93e62418fa6f265461010055b5000560.

--- src/NeoTime.h
+++ src/NeoTime.h
@@ -107,7 +107,8 @@ struct time_t {
 
   /**
    * Offset by a number of seconds.
-   * @param[in] offset in seconds.
+   * @param[in] seconds to offset.
+   * @return *this
    */
   void operator +=( clock_t offset )
     { *this = offset + operator clock_t(); }
@@ -192,7 +193,7 @@ struct time_t {
 
   /**
    * Determine the day of the week for the specified day number
-   * @param[in] dayno number as counted from January 1 of the epoch year.
+   * @param[in] day number as counted from January 1 of the epoch year.
    * @return weekday number 1..7, as for the /day/ member.
    */
   static uint8_t weekday_for(uint16_t dayno)
